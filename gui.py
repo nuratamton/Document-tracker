6f00e0c4ce1f5e0614b77e355881ddb121ccbd59
nrt2000 <nrt2000@hw.ac.uk>
2023-12-04 00:17:44 +0400

    commented GUI code

--- gui.py
+++ gui.py
@@ -32,26 +32,36 @@ def load_data(file, is_uploaded_file=False):
         print(f"Error reading file: {e}")
         return pd.DataFrame()
 
-
+# Function for the main page
 def main():
     st.title(''' :rainbow[Document Tracker] :bar_chart:''')
     st.subheader("Upload your file")
     default_file = "dataset.txt"
     data_file = st.file_uploader("Upload your JSON data", type=["json", "txt"], key="unique_file_uploader")
 
+    # Subheader for the displaying of data
     st.subheader("Visualized Data")
+    # if a file is already loaded
     if data_file is not None:
+        # load the file chosen by the user
         data = load_data(data_file, is_uploaded_file=True)
+        # save it
         st.session_state['data'] = data 
     else:
+        # if its not specified, load the default file
         data = load_data(default_file, is_uploaded_file=False)
+        # save it
         st.session_state['data'] = data 
 
+    # if data contains something
     if not data.empty:
+        # write the data
         st.write(data)
     else:
+        # if data was empty, show an error message
         st.error('Failed to load data. Please check the file format and contents.')
 
+    # Button to analyse the data
     st.button('Analyse data', on_click=navigate_to_options)
 
 # Function for options page
@@ -68,37 +78,53 @@ def options():
     col1, col2 = st.columns(2, gap="large")
     col3, col4 = st.columns(2, gap="large")
     
-
+    # in the first column
     with col1:
         # to load the image
-        image = Image.open("images/continent.jpg")
+        image = Image.open("images/continent.png")
         # displaying the image maintaing the column width
         st.image(image,use_column_width=True)
+        # A button to view by country/continent
         if st.button("View by Country/Continent"):
             st.session_state['task'] = 'View by Country/Continent'
             st.session_state['page'] = 'task_page'
-        
+    
+    # in the second column
     with col2:
+        # to load image
         image = Image.open("images/browser.png")
+        # display the image while maintaining the column width
         st.image(image,use_column_width=True)
+        # if button to view by browser is clicked
         if st.button("View by Browser"):
             # update session state
             st.session_state['task'] = 'View by Browser'
             print("Done")
             st.session_state['page'] = 'task_page'
     st.write("")
+
+    # in the third column
     with col3:
-        image = Image.open("images/profile.jpg")
+        # to load image
+        image = Image.open("images/profile.png")
         st.image(image,use_column_width=True)
         st.button("Reader profiles")
+
+    # in the fourth column
     with col4:
+        # to load image
         image = Image.open("images/likes.png")
+        # to display the image
         st.image(image,use_column_width=True)
+        # the button to Also Likes
         st.button("Also Likes", on_click="")
 
 # Funcition to navigate to options page
 def navigate_to_options():
+    # save the session state
     st.session_state['page'] = 'options'
+
+# Function to navigate to main page
 def navigate_to_main():
     st.session_state['page'] = 'main'
 
