7011cee6681450844a44a0d05b19d7f3c74d5512
sn20233 <90958275+sn20233@users.noreply.github.com>
2023-12-06 00:09:18 +0400

    Update view_by_browser.py

--- views/view_by_browser.py
+++ views/view_by_browser.py
@@ -5,6 +5,13 @@ from data_op.data_loader import Reader
 
 
 def broswer_count(data,doc_id):
+    import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from user_agents import parse
+from data_op.data_loader import Reader
+
+
+def broswer_count(data,doc_id):
     
     #filtering date based on document id given
     filter_document = data[data["env_doc_id"]== doc_id]
@@ -19,16 +26,42 @@ def broswer_count(data,doc_id):
 
     # calculating number of users for each browser used
     plot_data = final_data.groupby('visitor_useragent')['visitor_uuid'].nunique().reset_index()
-    print(plot_data)
 
-    #plotting the graph
+    #Storing browser name
+    browser_name= plot_data['visitor_useragent'].unique() 
+    # Storing number of users per browser 
+    user_count = plot_data['visitor_uuid']  
+
+    # storing data for plotting the histogram
+    hist_data = np.repeat(browser_name, user_count)
+
+    
     plt.figure(figsize=(5, 3))
-    ax = plt.bar(plot_data['visitor_useragent'], plot_data['visitor_uuid'], color='orange')
-    ax = plt.gca()  # Get the current Axes instance
-    ax.yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+
+    # mapping the values for graph
+    browser_to_number = {browser: i for i, browser in enumerate(browser_name)}
+    numerical_data = [browser_to_number[browser] for browser in hist_data]
+
+ 
+    ax = plt.hist(numerical_data, bins=np.arange(len(browser_name)+1)-0.7, color='green',edgecolor='black')
+
+    # Alligning labels
+    plt.gca().set_xticks(np.arange(len(browser_name)))
+
+    # Setting the x-axis labels 
+    plt.gca().set_xticklabels(browser_name, rotation=45, ha='right')
+
+    # Setting the y-axis values 
+    plt.gca().yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+
+    # Labeling the graph
     plt.xlabel('Browser')
     plt.ylabel('Number of Users')
     plt.title('Number of Users per Browser')
-    plt.xticks(rotation=45)
+
+   
+    plt.tight_layout()     
     return ax
+    
+
 
