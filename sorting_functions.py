6e95a18426711dbbb2d03d3bffcce1ce69e553f0
Gaurang Chitnis <gcc2000@hw.ac.uk>
2023-12-05 14:02:13 +0400

    also like 5 and 6

---
+++ sorting_functions.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+
+class sorting_doc_functions:
+
+    def __init__(self, data_frame):
+        self.data = data_frame
+ 
+
+    def sort_by_country_diversity_score(self,doc_list):
+        get_all_docs = self.data[self.data['subject_doc_id'].isin(doc_list)]
+        views_count_with_duplicate = get_all_docs['subject_doc_id'].value_counts()
+        country_diversity = get_all_docs.groupby('subject_doc_id')['visitor_country'].nunique()
+        scores = views_count_with_duplicate * country_diversity
+        return scores.sort_values(ascending=False).head(10).index.tolist()
+
+
+
+
+    
