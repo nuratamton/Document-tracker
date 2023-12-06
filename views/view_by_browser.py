c80b30c58b67d6489fe13e90f3589fa583c511a9
sn20233 <90958275+sn20233@users.noreply.github.com>
2023-12-06 16:17:12 +0400

    Update view_by_browser.py

--- views/view_by_browser.py
+++ views/view_by_browser.py
@@ -19,18 +19,18 @@ def broswer_count(data,doc_id):
     data = filter_document 
 
     #retrieving browser name removing uneccessary information using parser function from user_agent
-    data['visitor_useragent'] = data['visitor_useragent'].apply(lambda x: parse(x).browser.family)
+    data["visitor_useragent"] = data["visitor_useragent"].apply(lambda x: parse(x).browser.family)
 
     #removing repeating users using the same browser 
-    final_data = data.drop_duplicates(subset=['visitor_useragent', 'visitor_uuid'])  
+    final_data = data.drop_duplicates(subset=["visitor_useragent", "visitor_uuid"])  
 
     # calculating number of users for each browser used
-    plot_data = final_data.groupby('visitor_useragent')['visitor_uuid'].nunique().reset_index()
+    plot_data = final_data.groupby("visitor_useragent")["visitor_uuid"].nunique().reset_index()
 
     #Storing browser name
-    browser_name= plot_data['visitor_useragent'].unique() 
+    browser_name= plot_data["visitor_useragent"].unique() 
     # Storing number of users per browser 
-    user_count = plot_data['visitor_uuid']  
+    user_count = plot_data["visitor_uuid"]  
 
     # storing data for plotting the histogram
     hist_data = np.repeat(browser_name, user_count)
@@ -43,25 +43,72 @@ def broswer_count(data,doc_id):
     numerical_data = [browser_to_number[browser] for browser in hist_data]
 
  
-    ax = plt.hist(numerical_data, bins=np.arange(len(browser_name)+1)-0.7, color='green',edgecolor='black')
+    ax = plt.hist(numerical_data, bins=np.arange(len(browser_name)+1)-0.7, color="green",edgecolor="black")
 
     # Alligning labels
     plt.gca().set_xticks(np.arange(len(browser_name)))
 
     # Setting the x-axis labels 
-    plt.gca().set_xticklabels(browser_name, ha='center')
+    plt.gca().set_xticklabels(browser_name, ha="center")
 
     # Setting the y-axis values 
     plt.gca().yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
 
     # Labeling the graph
-    plt.xlabel('Browser')
-    plt.ylabel('Number of Users')
-    plt.title('Number of Users per Browser')
+    plt.xlabel("Browser")
+    plt.ylabel("Number of Users")
+    plt.title("Number of Users per Browser")
 
    
     plt.tight_layout()     
     return ax
+    
+def broswer_count_full(data,doc_id):
+    #filtering data by doc id
+    filter_document = data[data["subject_doc_id"]== doc_id]
+    data = filter_document 
+
+    final_data = data.drop_duplicates(subset=["visitor_useragent", "visitor_uuid"])   
+    
+    plot_data = final_data.groupby("visitor_useragent")["visitor_uuid"].nunique().reset_index()
+    print(plot_data)
+    #Storing browser name
+    browser_name= plot_data["visitor_useragent"].unique() 
+    # Storing number of users per browser 
+    user_count = plot_data["visitor_uuid"]  
+
+    # storing data for plotting the histogram
+    hist_data = np.repeat(browser_name, user_count)
+
+    
+    plt.figure(figsize=(5, 3))
+
+    # mapping the values for graph
+    browser_to_number = {browser: i for i, browser in enumerate(browser_name)}
+    numerical_data = [browser_to_number[browser] for browser in hist_data]
+
+ 
+    ax = plt.hist(numerical_data, bins=np.arange(len(browser_name)+1)-0.7, color="green",edgecolor="black")
+
+    # Alligning labels
+    plt.gca().set_xticks(np.arange(len(browser_name)))
+
+    # Setting the x-axis labels 
+    plt.gca().set_xticklabels(np.arange(len(browser_name)), ha="center")
+
+    # Setting the y-axis values 
+    plt.gca().yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+    #mapping the ind
+    index_table = pd.DataFrame({"Index_Number": np.arange(len(browser_name)), "Browser_Name": browser_name })
+
+    # Labeling the graph
+    plt.xlabel("Browser")
+    plt.ylabel("Number of Users")
+    plt.title("Number of Users per Browser")
+
+   
+    plt.tight_layout()     
+    return ax,index_table
     
 
 
