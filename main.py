b067fcf3dac9699ec6cf148931c7abfb4a099e7d
Gaurang Chitnis <gcc2000@hw.ac.uk>
2023-12-07 11:52:53 +0400

    cli done

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 import sys
 from tabulate import tabulate
+import matplotlib.image as mpimg
+import subprocess
 # from pandasgui import show
 # from gui import *
 from likes.also_like import AlsoLike
@@ -21,6 +23,9 @@ def main():
     # data = reader.load_data()
     pd = reader.concatenate_chunks()
     doc_id = args.doc_uuid
+    uid = args.user_uuid
+    if not pd['visitor_uuid'].isin([uid]).any():
+        uid = None
 
     if(args.task_id == "2"):
         country_continent = CountryContinent()
@@ -43,11 +48,23 @@ def main():
         print(tabulate(top_reader, headers = 'keys', tablefmt = 'psql'))
 
     elif(args.task_id == "5d"):
-        pass
+        also_like_obj = AlsoLike(pd)
+        also_liked = also_like_obj.get_also_like(doc_id,None,uid)
+        if(also_like.shape == (0,)):
+            print("There are no also liked docments.")
+        else:
+            plt.imshow(also_liked, cmap='gray')
+            plt.axis('off')
+            plt.show()
     elif(args.task_id == "6"):
-        pass
+        also_like = AlsoLike(pd)
+        generate_graph = also_like.generate_graph(doc_id,None,uid)
+        img = mpimg.imread(generate_graph+".png")
+        plt.imshow(img)
+        plt.axis('off')
+        plt.show()
     elif(args.task_id == "7"):
-        pass
+        subprocess.run(["streamlit", "run", "gui.py"])
     else:
         print("Please enter a valid task_ID ! \nArigato", file=sys.stderr)
 
